b0d7fafdef35bd27bd9166678816cf10ac67fe32
Nate <nate.strules@gmail.com>
2021-04-04 22:41:46 -0400

    Added basic test for service

--- PhotoAlbumTests/Service.cs
+++ PhotoAlbumTests/Service.cs
@@ -17,6 +17,19 @@ namespace PhotoAlbumTests
             var users = api.GetAllUserInfo();
 
             Assert.IsNotNull(users);
+            // assuming the service will always same numer of users (documentation indicates 10 are configured)
+            Assert.IsTrue(users.Count == 10);
+
+            // assuming service will always return a non-zero number of albums
+            Assert.IsTrue(users.TrueForAll(u => u.Albums != null));
+            Assert.IsTrue(users.TrueForAll(u => u.Albums.Count > 0));
+
+            // assuming service will always return a non-zero number of photos for each album
+            Assert.IsTrue(users.TrueForAll(u => u.Albums.TrueForAll(p => p.Photos.Count > 0)));
+
+            // assuming service will always return a non-zero number of posts for each user
+            Assert.IsTrue(users.TrueForAll(u => u.Posts != null));
+            Assert.IsTrue(users.TrueForAll(u => u.Posts.Count > 0));
         }
     }
 }
